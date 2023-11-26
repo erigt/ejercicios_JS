a3616e556743f3788aff7e4f3e2ed7fcc3b77af1
Erika <146768635+erigt@users.noreply.github.com>
2023-11-26 21:16:23 +0100

    cambios Ex1

--- src/Ex1/app.js
+++ src/Ex1/app.js
@@ -1,16 +1,15 @@
 /* Escribe un programa que escriba en la pantalla un texto de
  tipo <h1> que diga “Hello Javascript”. */
 
- // crear una variable Hello Javascript
- // imprimir el dato en etiqueta h1 en el DOM
-
+ // crear una variable que contenga el valor Hello Javascript.
+ 
 const title = "Hello Javascript"
 const titleSection = document.getElementById("title")
-
+//crear una función para insertar el title en la etiqueta h1.
 function printTitle() {
 	titleSection.innerHTML = /* html */ `
 	<h1>${title}</h1>
 	`
 }
-
+// imprimir el dato en etiqueta h1 en el DOM
 printTitle()

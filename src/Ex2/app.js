c64fa2f201379d2883f8a53c1998d67fca0db16e
Erika <146768635+erigt@users.noreply.github.com>
2023-11-26 21:43:15 +0100

    actualización del Ex2

--- src/Ex2/app.js
+++ src/Ex2/app.js
@@ -1,7 +1,11 @@
 /* Escribe un programa que escriba en la pantalla “La suma de 3 + 5 es <resultado>”.*/
 
-
-// crear variable de suma
+// creo dos variables (let) con los valores de 3 y 5.
+let a = 3;
+let b = 5;
+// creo una función sumar.
+function sumar(a,b) {
+	return (a+b);
+}
 // imprimir el dato en el DOM
-
-
+document.write("La suma de "+a+" + "+b+" es igual a " + sumar(a,b));

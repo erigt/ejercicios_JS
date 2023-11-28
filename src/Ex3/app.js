fa4a66d664f1ca802f5387afedffd45ee900a99b
Erika <146768635+erigt@users.noreply.github.com>
2023-11-28 21:54:13 +0100

    Guardo Ex3 finalizado + archivos preparados para próximos Ex

--- src/Ex3/app.js
+++ src/Ex3/app.js
@@ -1,13 +1,13 @@
 /* Escribe un programa que pida el nombre del usuario con un input 
 y escriba un texto que diga “Hola <nombre-de-usuario>”*/
 
-//creo
+//Creo una función saludar.
+//Dentro de la función creo una variable local.
+//Imprimo mensaje en el DOM conectado con HTML a través de: innerHTML.
 
-
-
-
-
-
+function saludar() {
+	let nombre = document.getElementById("nombre").value;
+	document.getElementById("saludo").innerHTML = "Hola " + nombre;
+}
 
 //imprimir el dato en el DOM
-
---
+++ src/Ex4/app.js
@@ -0,0 +1,2 @@
+/*Escribe un programa que pida dos números y escriba:
+ “La suma de <numero-uno> con <numero-dos> es <resultado>”.*/
